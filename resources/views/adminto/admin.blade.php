34ee525a5a8fa66506e596ab9e7cda106b0c8151
MD-NUR-MOHAMMAD <nurmohammad20161994@gmail.com>
2025-02-09 17:36:46 +0600

    Backend template setup

---
+++ resources/views/adminto/admin.blade.php
@@ -0,0 +1,12 @@
+@extends('adminto.layout.admin', ['title' => 'ecommerce', 'name' => 'dashboard'])
+
+@section('head')
+@endsection
+
+@section('content')
+<h1 class="text-center"> Admin Dashboard</h1>
+
+@endsection
+
+@section('script')
+@endsection
---
+++ resources/views/adminto/vendor.blade.php
@@ -0,0 +1,13 @@
+@extends('adminto.layout.vendor', ['title' => 'ecommerce', 'name' => 'vendor-dashboard'])
+@section('head')
+
+@endsection
+
+@section('content')
+<h1 class="text-center">Vendor Dashboard</h1>
+
+@endsection
+
+@section('script')
+
+@endsection
