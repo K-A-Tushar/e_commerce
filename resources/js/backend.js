e0c5ab6b5004fe3ad6c00bee2af030b31dd6730a
K-A-Tushar <katushar96@gmail.com>
2025-02-11 18:21:59 +0600

    #

--- resources/js/backend.js
+++ resources/js/backend.js
@@ -1,8 +1,5 @@
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import '@fortawesome/fontawesome-free/css/all.css';
 
 import Alpine from 'alpinejs';
 
